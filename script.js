2c6e8945ae8a296c06d4c6eb4be2b110b12c4ff1
Tomas Russo <trussoromera@gmail.com>
2022-08-15 17:44:09 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const divProductos = document.getElementById("divProductos")
+const divProductos = document.getElementById("tBody")
 
-fetch ('/json/productos.json')
+fetch ('./json/productos.json')
 .then(response => response.json())
-.then(data => console.log (data))
+.then(productos => {
+    productos.forEach((producto, indice) => {
+        tBody.innerHTML += `
+        <tr>
+        <th scope="row">${indice + 1 }</th>
+        <td>${producto.nombre}</td>  
+        <td>${producto.detalle}</td>
+        <td>${producto.precio}</td>
+        <td>${producto.stock}</td>
+        <td><img src="./multimedia/${producto.img}"></td>
+        <td><button class="btn btn-dark">Comprar</td>
+    </tr>
+        `
+    })
+     
+
+})
